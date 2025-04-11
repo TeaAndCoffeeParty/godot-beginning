2d6f892ffff987d3ac227349420cdbcffb41d40a
xunshichentuo <chentuo@sprintray.cn>
2025-04-11 21:48:21 +0800

    add button to triggole motion

--- firstgame/sprite_2d.gd
+++ firstgame/sprite_2d.gd
@@ -5,16 +5,20 @@ func _init():
 	print("Hello, World!")
 
 func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
+	#var direction = 0
+	#if Input.is_action_pressed("ui_left"):
+	#	direction = -1
+	#if Input.is_action_pressed("ui_right"):
+	#	direction = 1
 	
-	rotation += angular_speed * delta * direction
+	rotation += angular_speed * delta
 	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation)*speed
-#	var velocity = Vector2.UP.rotated(rotation) * speed
+	#var velocity = Vector2.ZERO
+	#if Input.is_action_pressed("ui_up"):
+	#	velocity = Vector2.UP.rotated(rotation)*speed
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity *delta
+
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing())

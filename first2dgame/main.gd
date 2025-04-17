eb481eb3b20a97ec755c8d9b58ce7e28e1c071ca
xunshichentuo <chentuo@sprintray.cn>
2025-04-17 21:57:03 +0800

    add music and shortcut for start button

--- first2dgame/main.gd
+++ first2dgame/main.gd
@@ -13,7 +13,9 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	$HUD.show_game_over()	
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -22,6 +24,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_score_timer_timeout() -> void:
 	score += 1

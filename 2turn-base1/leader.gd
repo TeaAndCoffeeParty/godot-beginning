ead4d19e18ec2bc89d37438a8be3a972427e84da
Chen Tuo <ch122633@126.com>
2025-08-19 22:50:15 +0800

    add previous button record

--- 2turn-base1/leader.gd
+++ 2turn-base1/leader.gd
@@ -5,6 +5,7 @@ signal followMe()
 var direction = Vector2(1, -0.5)
 var maxSpeed = 2
 var currentDir
+var previousDir
 
 func _physics_process(delta: float) -> void:
 	var speed = maxSpeed
@@ -24,11 +25,34 @@ func _physics_process(delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
+		if previousDir != currentDir:
+			previousDir = currentDir
 		currentDir = "TopRight"
 	elif event.is_action_pressed("ui_right"):
+		if previousDir != currentDir:
+			previousDir = currentDir
 		currentDir = "DownRight"
 	elif event.is_action_pressed("ui_down"):
+		if previousDir != currentDir:
+			previousDir = currentDir
 		currentDir = "DownLeft"
 	elif event.is_action_pressed("ui_left"):
+		if previousDir != currentDir:
+			previousDir = currentDir
 		currentDir = "TopLeft"
+	
+	var u = event.is_action_released("ui_up")
+	var r = event.is_action_released("ui_right")
+	var d = event.is_action_released("ui_down")
+	var l = event.is_action_released("ui_left")
+
+	if u and currentDir == "TopRight":
+		currentDir = previousDir
+	if r and currentDir == "DownRight":
+		currentDir = previousDir
+	if d and currentDir == "DownLeft":
+		currentDir = previousDir
+	if l and currentDir == "TopLeft":
+		currentDir = previousDir
+
 	pass

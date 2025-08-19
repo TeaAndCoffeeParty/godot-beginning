4c9d024fc458e2303e0f7089b021040f9dd76437
Chen Tuo <ch122633@126.com>
2025-08-19 22:33:12 +0800

    记住最后按下的方向键

--- 2turn-base1/leader.gd
+++ 2turn-base1/leader.gd
@@ -4,21 +4,31 @@ signal followMe()
 
 var direction = Vector2(1, -0.5)
 var maxSpeed = 2
+var currentDir
 
 func _physics_process(delta: float) -> void:
 	var speed = maxSpeed
-	if Input.is_action_pressed("ui_up"):
+	if currentDir == "TopRight" and Input.is_action_pressed("ui_up"):
 		direction = Vector2(1, -0.5)
-	elif Input.is_action_pressed("ui_right"):
+	elif currentDir == "DownRight" and Input.is_action_pressed("ui_right"):
 		direction = Vector2(1, 0.5)
-	elif Input.is_action_pressed("ui_down"):
+	elif currentDir == "DownLeft" and Input.is_action_pressed("ui_down"):
 		direction = Vector2(-1, 0.5)
-	elif Input.is_action_pressed("ui_left"):
+	elif currentDir == "TopLeft" and Input.is_action_pressed("ui_left"):
 		direction = Vector2(-1, -0.5)
 	else:
 		speed = 0
 
 	global_position += direction * speed
 	emit_signal("followMe", direction, speed, global_position, direction*speed)
-	#velocity = direction * speed
-	#move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up"):
+		currentDir = "TopRight"
+	elif event.is_action_pressed("ui_right"):
+		currentDir = "DownRight"
+	elif event.is_action_pressed("ui_down"):
+		currentDir = "DownLeft"
+	elif event.is_action_pressed("ui_left"):
+		currentDir = "TopLeft"
+	pass

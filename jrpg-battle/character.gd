b586d3da49f170c13f084938df9425ff79b99cb1
Chen Tuo <61954058+soonsolidchentuo@users.noreply.github.com>
2025-11-25 10:41:47 +0800

    update lib interface (#3)
    Co-authored-by: Tuo <ch122633@126.com>

--- jrpg-battle/character.gd
+++ jrpg-battle/character.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-@export var character_name: String = "Character"
-@export var hp: int = 100
-@export var max_hp: int = 100
-@export var attack: int = 10
+var core: PlayerGD
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var name_label: Label = $Label
@@ -11,22 +8,30 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	core = PlayerGD.new()
 	update_ui()
 	pass # Replace with function body.
 
-	
+func get_attack() -> int:
+	if core:
+		return core.get_attack()
+	return 0
+
 func take_damage(damage_amount: int):
-	hp -= damage_amount
-	hp = max(0, hp)
-	print("%s takes %d damage. HP is now %d" % [character_name, damage_amount, hp])
+	core.take_damage(damage_amount)
+	print("%s takes %d damage. HP is now %d" % [core.get_name(), damage_amount, core.get_current_hp()])
 	update_ui()
 
 func update_ui():
+	var current_hp = core.get_current_hp()
+	var max_hp = core.get_max_hp()
+	var character_name = core.get_name()
+
 	if progress_bar:
 		progress_bar.max_value = max_hp
-		progress_bar.value = hp
+		progress_bar.value = current_hp
 	if name_label:
-		name_label.text = "%s\nHP: %d/%d" % [character_name, hp, max_hp]
+		name_label.text = "%s\nHP: %d/%d" % [character_name, current_hp, max_hp]
 
 func is_dead() -> bool:
-	return hp <= 0
+	return !core.is_alive()

6792af1232d69bcd0bba7596e39cff2d344bb8dd
Tuo <ch122633@126.com>
2026-01-10 23:02:46 +0800

    add idle initialize status

--- tutorial-move/Characters/player_cat.gd
+++ tutorial-move/Characters/player_cat.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
-@export var move_speed :float = 100
+@export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+
+func _ready() -> void:
+#	animation_tree.set("parameters/Idle/blend_position", starting_direction)
+	animation_tree.set("parameters/Walk/blend_position", starting_direction)
+	pass
+
 func _physics_process(delta: float) -> void:
 	var input_direction = Vector2(
 		Input.get_action_strength('right') - Input.get_action_strength('left'),
 		Input.get_action_strength('down') - Input.get_action_strength('up')
 	)
-	
+
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+	pass
